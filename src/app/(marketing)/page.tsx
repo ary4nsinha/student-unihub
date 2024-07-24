b6c3682cfae68ff1558806666bf47f8da09ad179
ary4nsinha <115373151+ary4nsinha@users.noreply.github.com>
2024-07-24 21:16:17 +0530

    tried optimizing landing page

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,19 +1,22 @@
-import ContentBlock from "@/components/contentBlock";
-import H1 from "@/components/h1";
-import Logo from "@/components/logo";
-
-import { SignedOut, SignInButton } from "@clerk/nextjs";
-
+import { Inter } from "next/font/google";
 import Image from "next/image";
+import dynamic from "next/dynamic";
+import H1 from "@/components/h1";
+import { SignedOut, SignInButton } from "@clerk/nextjs";
 import Link from "next/link";
 import React from "react";
 
+const inter = Inter({ subsets: ["latin"] });
+const DynamicContentBlock = dynamic(() => import("@/components/contentBlock"));
+
 export default function Page() {
   return (
-    <div className="flex bg-[#2C9676] min-h-screen items-center justify-center flex-col max-w-[1050px] mx-auto px-4 ">
-      <ContentBlock className="height-[450px] w-fit flex flex-col lg:flex-row xl:flex-row items-center justify-center gap-10 p-4 py-12">
+    <div
+      className={`flex bg-[#2C9676] min-h-screen items-center justify-center flex-col max-w-[1050px] mx-auto px-4 ${inter.className}`}
+    >
+      <DynamicContentBlock className="height-[450px] w-fit flex flex-col lg:flex-row xl:flex-row items-center justify-center gap-10 p-4 py-12">
         <div className="p-2">
-          <Image width={230} height={230} src="/logo.svg" alt="logo" />
+          <Image priority width={230} height={230} src="/logo.svg" alt="logo" />
         </div>
         <div className="items-center justify-center space-y-4 text-center">
           <H1 className="text-4xl font-bold">
@@ -37,7 +40,7 @@ export default function Page() {
             </div>
           </div>
         </div>
-      </ContentBlock>
+      </DynamicContentBlock>
     </div>
   );
 }
